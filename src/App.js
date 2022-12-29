090980138f28e6c82ddd15d7675bce5302852690
gussXX <gus7avo182@gmail.com>
2022-12-28 22:43:01 -0300

    Add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useRef } from 'react';
 
-function App() {
+function App2() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ola mundo</h1>
     </div>
   );
+}
+
+const App = () => {
+
+    const [name, setname] = React.useState('Valor')
+    React.useEffect(()=>{
+      console.log('useEffect com dependecias vazias')
+    }, [])
+    React.useEffect(()=>{
+      console.log('useEffect com dependecia [name]')
+    }, [name])
+    React.useEffect(()=>{
+      console.log('useEffect sem dependencias')
+    })
+    console.log('render')
+
+  const [value, setValue] = useState('')
+  const [data, setData] = useState([])
+  const inputEl = useRef(null)
+
+  const handclick = () => {
+
+    if (value == '') {
+      console.log('Inputbox vazia !');
+    } else {
+      setData(prev =>[...prev, value])
+    }
+    setValue('')
+  }
+
+  return(
+    <div class="App">
+      <h1>Hello word!</h1>
+      <p>{name}</p>
+      <button onClick={null}>Alterar variavel</button>
+
+      <br></br>
+      
+      <input ref={inputEl} type="text" value={value} onChange={event => setValue(event.target.value)}></input>
+      <button onClick={handclick}>Add</button>
+      <div>
+        <ul>
+          {
+            data.map((item, index)  =>(
+              <li key={index}>
+                {item}
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default App;
